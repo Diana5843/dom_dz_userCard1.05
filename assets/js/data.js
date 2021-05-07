317e7b46ef545f893b5c1579e7cae837383fa154
Diana Sokolova <dianasokolova5843@gmail.com>
2021-05-07 12:54:57 +0300

    init commit

---
+++ assets/js/data.js
@@ -0,0 +1,70 @@
+"use strict";
+/**
+ Constants
+ */
+ const socials = {
+  "www.facebook.com" : "fab fa-facebook-f",
+  "twitter.com" : "fab fa-twitter",
+  "www.instagram.com" : "fab fa-instagram"
+};
+
+
+
+const responseData = [
+  {
+    id: 1,
+    firstName: 'Nelson',
+    lastName: 'Mandela',
+    profilePicture:
+      'https://www.hellomagazine.com/imagenes/celebrities/2013120513197/nelson-mandela-famous-quotes/0-82-581/mandela--z.jpg',
+    contacts: [
+      'https://www.facebook.com/',
+      'https://www.twitter.com/',
+      'https://www.instagram.com/',
+    ],
+  },
+  {
+    id: 2,
+    firstName: 'Leonardo',
+    lastName: 'DiCaprio',
+    profilePicture:
+      'https://www.toptenthebest.com/wp-content/uploads/2015/03/7.-Leonardo-DiCaprio.jpeg',
+    contacts: [
+      'https://www.facebook.com/LeonardoDiCaprio',
+      'https://www.instagram.com/leonardodicaprio/?hl=ru',
+    ],
+  },
+  {
+    id: 3,
+    firstName: 'Will',
+    lastName: 'Smith',
+    profilePicture:
+      'https://www.webbspy.com/wp-content/uploads/2019/11/Will-Smith.jpg',
+    contacts: [
+      'https://www.facebook.com/WillSmith',
+      'https://www.instagram.com/willsmith/?hl=ru',
+    ],
+  },
+  {
+    id: 4,
+    firstName: 'Chris',
+    lastName: 'Hemsworth',
+    profilePicture: 'https://www.top10about.com/wp-content/uploads/2018/01/Chris-Hemsworth.jpg',
+    contacts: [
+      'https://twitter.com/chrishemsworth',
+      'https://www.instagram.com/chrishemsworth/?hl=ru',
+    ],
+  },
+  {
+    id: 5,
+    firstName: 'Johnny',
+    lastName: 'Depp',
+    profilePicture:
+      'https://img.gazeta.ru/files3/853/12972853/upload-2020-02-21T212216Z_1531801277_RC285F9PAV8N_RTRMADP_3_FILMFESTIVAL-BERLIN-MINAMATA-RED-CARPET-pic4_zoom-1500x1500-94129.jpg',
+    contacts: [
+      'https://www.facebook.com/JohnnyDepp',
+      'https://twitter.com/search?q=%23justiceforjohnnydepp',
+      'https://www.instagram.com/johnnydepp/?hl=ru',
+    ],
+  },
+];
